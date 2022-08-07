fde502fe6b29577152eb6ad769288af3226a78db
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-07 14:36:46 -0400

    genre model with interface

---
+++ src/models/genre.ts
@@ -0,0 +1,15 @@
+import { Schema, model } from "mongoose"
+
+interface IGenre {
+  name: string
+}
+const GenreSchema = new Schema<IGenre>({
+  name: { type: String, required: true, minLength: 3, maxLength: 100 },
+})
+
+GenreSchema.virtual("url").get(function () {
+  return "/catalog/genre/" + this._id
+})
+
+const Genre = model<IGenre>("Genre", GenreSchema)
+module.exports = Genre
