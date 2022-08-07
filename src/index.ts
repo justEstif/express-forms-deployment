923a997bf375d0b3a494b70e26833cee67005d1b
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-07 14:37:21 -0400

    working mongo and port import

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,27 @@
-import express from "express"
-import path from "path"
-import mongoose from "mongoose"
+import express from "express";
+import path from "path";
+import mongoose from "mongoose";
 
-import { config } from "./port-config"
-import indexRouter from "./routes"
-
-const MONGODB_URI =  "mongodb+srv://justEstif:justEstifpw123@cluster0.c5ompsq.mongodb.net/local_library?retryWrites=true&w=majority"
+import endpoints from "./endpoints.config";
+import indexRouter from "./routes";
 
 // Connect to MongoDB
-mongoose.connect(MONGODB_URI)
+mongoose.connect(endpoints.MONGO_URI);
 
-const app: express.Express = express()
-const port = config.server.port
+const app: express.Express = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "..", "views"))
-app.set("view engine", "pug")
+app.set("views", path.join(__dirname, "..", "views"));
+app.set("view engine", "pug");
 
 //Body Parser Middleware
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use("/", indexRouter)
+app.use("/", indexRouter);
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
-})
+app.listen(endpoints.PORT, () => {
+  console.log(
+    `⚡️[server]: Server is running at https://localhost:${endpoints.PORT}`
+  );
+});
