44f00f5c5c8189ef18e750842c370ea083fe9932
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-07 17:23:42 -0400

    removed userrouter

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,29 @@
-import express from "express";
-import path from "path";
-import mongoose from "mongoose";
+import express from "express"
+import path from "path"
+import mongoose from "mongoose"
 
-import endpoints from "./endpoints.config";
-import indexRouter from "./routes";
+import endpoints from "./endpoints.config"
+import indexRouter from "./routes"
+import catalogRouter from "./routes/catalog"
 
 // Connect to MongoDB
-mongoose.connect(endpoints.MONGO_URI);
+mongoose.connect(endpoints.MONGO_URI)
 
-const app: express.Express = express();
+const app: express.Express = express()
 
 // view engine setup
-app.set("views", path.join(__dirname, "..", "views"));
-app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "..", "views"))
+app.set("view engine", "pug")
 
 //Body Parser Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
-app.use("/", indexRouter);
+app.use("/", indexRouter)
+app.use("/catalog", catalogRouter)
 
 app.listen(endpoints.PORT, () => {
   console.log(
     `⚡️[server]: Server is running at https://localhost:${endpoints.PORT}`
-  );
-});
+  )
+})
