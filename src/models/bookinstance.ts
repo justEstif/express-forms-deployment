645294d145c6d1c93a2932087b6f557288fac9fe
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-07 14:36:40 -0400

    book instance model with interface

---
+++ src/models/bookinstance.ts
@@ -0,0 +1,27 @@
+import { Schema, model, Types } from "mongoose"
+
+interface IBookInstance {
+  book: Types.ObjectId
+  imprint: string
+  status: "Available" | "Maintenance" | "Loaned" | "Reserved"
+  due_back: Date
+}
+
+const BookInstanceSchema = new Schema<IBookInstance>({
+  book: { type: Schema.Types.ObjectId, ref: "Book", required: true },
+  imprint: { type: String, required: true },
+  status: {
+    type: String,
+    required: true,
+    enum: ["Available", "Maintenance", "Loaned", "Reserved"],
+    default: "Maintenance",
+  },
+  due_back: { type: Date, default: Date.now },
+})
+
+BookInstanceSchema.virtual("url").get(function () {
+  return "/catalog/bookinstance/" + this._id
+})
+
+const BookInstance = model<IBookInstance>("BookInstance", BookInstanceSchema)
+module.exports = BookInstance
