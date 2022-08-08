29ac52f103de7021e12692b8d34d1db2b97d0bd5
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-08 08:06:45 -0400

    create the author detail request handler

--- src/controllers/authorController.ts
+++ src/controllers/authorController.ts
@@ -1,6 +1,8 @@
 import { RequestHandler } from "express"
+import async from "async"
 
 import Author from "../models/author"
+import Book from "../models/book"
 
 // Display list of all Authors.
 export const author_list: RequestHandler = (_, res, next) => {
@@ -16,8 +18,33 @@ export const author_list: RequestHandler = (_, res, next) => {
 }
 
 // Display detail page for a specific Author.
-export const author_detail: RequestHandler = (_, res) => {
-  res.send(`NOT IMPLEMENTED: Author detail: ${_.params.id}`)
+export const author_detail: RequestHandler = (req, res, next) => {
+  async.parallel(
+    {
+      // get author and books of author
+      author(callback) {
+        Author.findById(req.params.id).exec(callback)
+      },
+      authors_books(callback) {
+        Book.find({ author: req.params.id }, "title summary").exec(callback)
+      },
+    },
+    function(err, results) {
+      // Error in API usage.
+      if (err) return next(err)
+      else if (results.author == null) {
+        const err = new Error("Author not found")
+        res.status(404)
+        return next(err)
+      } else {
+        res.render("author_detail", {
+          title: "Author Detail",
+          author: results.author,
+          author_books: results.authors_books,
+        })
+      }
+    }
+  )
 }
 
 // Display Author create form on GET.
