103672ec141aa3899a5a49ccdda3f017190e11d5
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-08 04:02:15 -0400

    changed index response in book controller

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -1,8 +1,37 @@
-// import Book from "../models/book"
+import async from "async"
+import Book from "../models/book"
+import Author from "../models/author"
+import Genre from "../models/genre"
+import BookInstance from "../models/bookinstance"
 import { RequestHandler } from "express"
 
 export const index: RequestHandler = (_, res) => {
-  res.send("NOT IMPLEMENTED: Site Home Page")
+  async.parallel(
+    {
+      book_count(callback) {
+        Book.countDocuments({}, callback) // Pass an empty object as match condition to find all documents of this collection
+      },
+      book_instance_count(callback) {
+        BookInstance.countDocuments({}, callback)
+      },
+      book_instance_available_count(callback) {
+        BookInstance.countDocuments({ status: "Available" }, callback)
+      },
+      author_count(callback) {
+        Author.countDocuments({}, callback)
+      },
+      genre_count(callback) {
+        Genre.countDocuments({}, callback)
+      },
+    },
+    function (err, results) {
+      res.render("index", {
+        title: "Local Library Home",
+        error: err,
+        data: results,
+      })
+    }
+  )
 }
 
 // Display list of all books.
