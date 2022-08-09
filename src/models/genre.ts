dfc6fe2a367cd36947863314cfd6dd91e7afd3bd
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-09 07:01:23 -0400

    exporting interface

--- src/models/genre.ts
+++ src/models/genre.ts
@@ -1,19 +1,19 @@
-import { Schema, model, Types } from "mongoose";
+import { Schema, model, Types } from "mongoose"
 
 interface IGenre {
-  name: string;
-  url: string;
-  checked: string;
-  _id: Types.ObjectId;
+  name: string
+  url: string
+  checked: string
+  _id: Types.ObjectId
 }
 const GenreSchema = new Schema<IGenre>({
   name: { type: String, required: true, minLength: 3, maxLength: 100 },
-});
+})
 
 GenreSchema.virtual("url").get(function () {
-  return "/catalog/genre/" + this._id;
-});
+  return "/catalog/genre/" + this._id
+})
 
-const Genre = model<IGenre>("Genre", GenreSchema);
-export { IGenre };
-export default Genre;
+const Genre = model<IGenre>("Genre", GenreSchema)
+export { IGenre }
+export default Genre
