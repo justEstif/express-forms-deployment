5ac668a88f4a110effabbee67fbbf91b7a79bbf8
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-11 03:59:11 -0400

    added get and post request for bookinstance update route

--- src/controllers/bookinstanceController.ts
+++ src/controllers/bookinstanceController.ts
@@ -1,7 +1,8 @@
 import { RequestHandler, Response, Request, NextFunction } from "express"
 import { body, validationResult } from "express-validator"
+import async from "async"
 
-import BookInstance from "../models/bookinstance"
+import BookInstance, { IBookInstance } from "../models/bookinstance"
 import Book, { IBook } from "../models/book"
 
 // Display: Request list of _ BookInstances.
@@ -131,11 +132,85 @@ export const bookinstance_delete_post: RequestHandler = (req, res, next) => {
 }
 
 // Display BookInstance update form on GET.
-export const bookinstance_update_get: RequestHandler = (_, res) => {
-  res.send("NOT IMPLEMENTED: BookInstance update GET")
+export const bookinstance_update_get: RequestHandler = (req, res, next) => {
+  // Get book, authors and genres for form.
+  async.parallel(
+    {
+      bookinstance: (callback) => {
+        BookInstance.findById(req.params.id).populate("book").exec(callback)
+      },
+      books: (callback) => {
+        Book.find(callback)
+      },
+    },
+    // show form if no err or null
+    (err, results: IBookInstance | any) => {
+      if (err) return next(err)
+      else if (results.bookinstance == null) {
+        const err = new Error("Book copy not found")
+        res.status(404)
+        return next(err)
+      } else {
+        res.render("bookinstance_form", {
+          title: "Update  BookInstance",
+          book_list: results.books,
+          selected_book: results.bookinstance.book._id,
+          bookinstance: results.bookinstance,
+        })
+      }
+    }
+  )
 }
 
-// Handle bookinstance update on POST.
-export const bookinstance_update_post: RequestHandler = (_, res) => {
-  res.send("NOT IMPLEMENTED: BookInstance update POST")
-}
+export const bookinstance_update_post = [
+  // Validate and sanitize fields.
+  body("book", "Book must be specified").trim().isLength({ min: 1 }).escape(),
+  body("imprint", "Imprint must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("status").escape(),
+  body("due_back", "Invalid date")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+
+  // Process request after validation and sanitization.
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req)
+    const bookinstance = new BookInstance({
+      book: req.body.book,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back,
+      _id: req.params.id,
+    })
+
+    // if errors, render again with form values
+    if (!errors.isEmpty()) {
+      Book.find({}, "title").exec((err, books) => {
+        if (err) return next(err)
+        else {
+          res.render("bookinstance_form", {
+            title: "Update BookInstance",
+            book_list: books,
+            selected_book: bookinstance.book._id,
+            errors: errors.array(),
+            bookinstance: bookinstance,
+          })
+        }
+      })
+      return
+    } else {
+      BookInstance.findByIdAndUpdate(
+        req.params.id,
+        bookinstance,
+        {},
+        (err, thebookinstance: IBookInstance | any) => {
+          if (err) return next(err)
+          else res.redirect(thebookinstance.url)
+        }
+      )
+    }
+  },
+]
